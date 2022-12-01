3068157970ab203e27e28c2b20e3c3fda4282c86
VuVanLien1503 <vanlien.vu1503@gmail.com>
2022-12-01 14:35:47 +0700

    Khai Báo Và Sử Dụng Biến

--- src/SystemTime.java
+++ src/SystemTime.java
@@ -4,5 +4,17 @@ public class SystemTime {
     public static void main(String[] args) {
         Date now = new Date();
         System.out.println(now);
+
+        int i=10;
+        float f =20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'c';
+        String s ="String";
+        System.out.println(" int :"+i);
+        System.out.println(" double :"+f);
+        System.out.println(" boolean :"+d);
+        System.out.println(" char :"+c);
+        System.out.println(" String :"+s);
     }
 }
